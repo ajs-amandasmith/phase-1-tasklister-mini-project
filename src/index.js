b75c5a139fe1015390d551cda38236788aa186b5
Amanda Smith <ajs.amandasmith@gmail.com>
2022-04-24 14:57:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target['new-task-description'].value);
+    form.reset();
+  });
+
+  function buildToDo(todo) {
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+    btn.addEventListener('click', handleDelete);
+    btn.textContent = 'Delete';
+    p.textContent = `${todo} `;
+    p.appendChild(btn);
+    document.querySelector('#tasks').appendChild(p);
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove();
+  }
 });
+
+// takes input
+// hit submit
+// appears on browser
+// delete button to remove task
+
+
+// grab the form and add an event listener (submit)
